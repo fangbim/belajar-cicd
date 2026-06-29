a290b92b3be75f1c6c399f1a1ca59ef53a55c2ff
fangbim <fang.bim891@gmail.com>
2026-06-29 14:00:47 +0700

    update port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 3023;
 
 app.get('/', (req, res) => {
     res.send('Deploy otomatis berhasil! Ini versi 2.0 tanpa sentuh VPS');
